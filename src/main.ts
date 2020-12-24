3cdf616a3067eb9880cb4d886233384a964426a9
xueqingxiao <xueqingxiao@gmail.com>
2020-12-24 16:47:27 +0800

    fix: listen port

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const __PROD__ = process.env.NODE_ENV === 'production';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
   app.enableCors();
-  await app.listen(__PROD__ ? 80 : 3030);
+  await app.listen(process.env.PORT ?? 3030);
 }
 bootstrap();
