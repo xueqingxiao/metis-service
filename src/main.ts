706f72b99d8fc449a7aeb9c3b5aa72bb44672972
xueqingxiao <xueqingxiao@gmail.com>
2020-12-24 11:56:17 +0800

    chore: 80 port on production

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const __PROD__ = process.env.NODE_ENV === 'production';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
   app.enableCors();
-  await app.listen(3030);
+  await app.listen(__PROD__ ? 80 : 3030);
 }
 bootstrap();
